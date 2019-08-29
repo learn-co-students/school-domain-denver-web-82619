66b6acf8b4fa77f1b2b5142eb35ec31ecbfac1b9
arinavictor <arinavictor@yahoo.com>
2019-08-29 14:23:31 -0600

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,19 @@
 # code here!
+class School 
+
+    def initialize(name)
+    
+    end 
+
+    def roster
+        new = {}
+    end 
+
+    def add_student(name, grade)
+        new
+         => {"name" => ["grade"]}
+
+    end 
+end 
+
+school = School.new("Bayside High School")
